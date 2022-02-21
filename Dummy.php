c1fb7c3551dd279b460451811c0cb1aea68881e5
moeenhaqqani <100119666+moeenhaqqani@users.noreply.github.com>
2022-02-21 20:05:14 +0500

    Update Dummy.php
    Changes made in dummy

--- Dummy.php
+++ Dummy.php
@@ -1,10 +1,10 @@
 <?php
 
 class Dummy
-{\\this is the file that is edited by me.
+{ 
     public function getValue()
     {
         //$result->addAttributeToFilter('price', array('gteq' => 19));
-        return 2;
+        return 2332;
     }
 }
